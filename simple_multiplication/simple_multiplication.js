3f78a8a64e8719f039339266ce55164b26e6e7e2
9ine <A.Dot@9ine.local>
2019-03-07 16:13:23 -0500

    Commiting simple_multiplicationg

---
+++ simple_multiplication/simple_multiplication.js
@@ -0,0 +1,11 @@
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(num) {
+  if( num % 2 == 0 ){
+    return(num * 8);
+    //if what is before this is false run this
+    }else{
+      return (num * 9);
+    }
+  
+};
