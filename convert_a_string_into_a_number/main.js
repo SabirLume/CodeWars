677f864df9964ba2f1b94bb1e9b52383fe75b5dc
9ine <A.Dot@9ine.local>
2019-03-08 21:12:01 -0500

    Adding Convert a string into a number

---
+++ convert_a_string_into_a_number/main.js
@@ -0,0 +1,6 @@
+//Function that can tranasfer a string into a number
+var stringToNumber = function(str){
+//Method that takes string and turn into number
+//return a string as a number 
+      return parseInt(str) 
+    }
