807d8f83446c1b47fe5b9706d0d150584f1211b5
9ine <A.Dot@9ine.local>
2019-03-30 13:56:06 -0400

    Grasshopper Summation & Determine off spring sex codewars

---
+++ determine_offspring_sex/main.js
@@ -0,0 +1,7 @@
+function chromosomeCheck(sperm) {
+    if(sperm == 'XY'){
+    return "Congratulations! You're going to have a son."
+    }else{
+    return "Congratulations! You're going to have a daughter."
+    }
+  }
