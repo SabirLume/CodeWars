cf6d2bf624c5212d453f959991e77c8372b7926c
SabirLume <A.Dot@9ine.local>
2019-04-02 23:41:13 -0400

    Binary Addition

---
+++ binary_additional/main.js
@@ -0,0 +1,14 @@
+
+function addBinary(a,b){
+ return (a+b).toString(2)
+}
+
+// Test.describe("addBinary(1,2)", function() {
+//   var results1 = addBinary(1,2);
+//   Test.it("Should return something that isn't falsy", function() {
+//     Test.expect(results1, "Something is wrong, no results!");
+//   });
+//   Test.it("Should return \"11\"", function() {
+//     Test.assertEquals(results1, "11");
+//   });
+// });
