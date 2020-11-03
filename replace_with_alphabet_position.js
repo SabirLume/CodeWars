a5ec98050b8147dcb15376dd0d7c288d4018d175
SabirLume <sabirlume@gmail.com>
2020-11-03 18:22:54 -0500

    simple 8kyu

---
+++ replace_with_alphabet_position.js
@@ -0,0 +1,40 @@
+// In this kata you are required to, given a string, replace every 
+// letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+function alphabetPosition(text) {
+    let lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+    let upperCase = lowerCase.toUpperCase();
+    let regex = /[A-z]/g;
+    let key = [];
+    let result = '';
+    
+    let inputArray = text.match(regex) ? text.match(regex) : [];
+    lowerCase = lowerCase.match(regex);
+    upperCase = upperCase.match(regex);
+  
+  
+    
+    //create pairs for A-z ex. [[a,A],[b,B]...]
+    lowerCase.forEach((letter, index) => {
+       key.push([lowerCase[index], upperCase[index]]);
+    })
+  
+    inputArray.forEach((letter, index) => {
+      key.forEach((subArray, subIndex) => {
+        if(letter === subArray[0] || letter === subArray[1]) {
+          result = result.concat((subIndex + 1) + ' ');
+        }
+      })
+    })
+                
+    return result.trim()
+  
+  }
